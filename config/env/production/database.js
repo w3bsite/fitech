8610c2d647cb74ebb44ee858b6a29d8944c919a1
w3bsite <61429140+w3bsite@users.noreply.github.com>
2021-01-03 08:03:34 +0330

    Update database

---
+++ config/env/production/database.js
@@ -0,0 +1,24 @@
+const config = parse(process.env.DATABASE_URL);
+
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false
+        }
+      },
+      options: {
+        ssl: true,
+      },
+    },
+  },
+});
